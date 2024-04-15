af31a10931bfcb043fd3d6adc0264709294f4fce
Vitor221 <vitornascimento6650@gmail.com>
2024-04-15 20:06:58 -0300

    creating route Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'prefix',
+    redirectTo: 'login'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
