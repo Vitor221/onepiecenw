984f700c8c59a431beeb641be9cf3fbd5c465341
Vitor221 <vitornascimento6650@gmail.com>
2024-04-16 19:05:58 -0300

    fix background in login

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,10 +1,14 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { MenubarComponent } from './components/menubar/menubar.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ NgOptimizedImage ],
+  imports: [
+    NgOptimizedImage,
+    MenubarComponent
+   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
