831bc55dc1e444bdfde1f2df0e6885a4ae0ef7c6
Vitor221 <vitornascimento6650@gmail.com>
2024-04-17 21:28:53 -0300

    Create component Register and RouteLink entry Login and Register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/pages/login/login.component';
+import { RegisterComponent } from './components/pages/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     LoginComponent,
+    RegisterComponent,
+    RouterModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,13 +1,15 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MenubarComponent } from './components/menubar/menubar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    MenubarComponent
+    MenubarComponent,
+    RouterModule
    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
