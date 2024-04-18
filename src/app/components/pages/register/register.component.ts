f72e60d36fecf5a6860ed0b64338a63a728587eb
Vitor221 <vitornascimento6650@gmail.com>
2024-04-18 01:42:00 -0300

    Heroicons implement

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,11 +1,13 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgHeroiconsModule } from '@dimaslz/ng-heroicons';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgHeroiconsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
